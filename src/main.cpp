12c2ca8b412fb61c5c645a4ea19e8b7e693b3a63
Tran Phuoc Dung Dang <dangphuocdung07@gmail.com>
2026-01-24 22:11:32 -0500

    mouse moving features

--- src/main.cpp
+++ src/main.cpp
@@ -1,18 +1,15 @@
+#include <Wire.h>    //esp32 SDA=21,SCL=22
+#include <BleMouse.h>
 #include <Arduino.h>
-
-// put function declarations here:
-int myFunction(int, int);
+#include "mouse-control.h"
 
 void setup() {
-  // put your setup code here, to run once:
-  int result = myFunction(2, 3);
-}
+  setupMouse();
 
+
+}
 void loop() {
-  // put your main code here, to run repeatedly:
-}
+  loopMouse();
 
-// put function definitions here:
-int myFunction(int x, int y) {
-  return x + y;
+  //
 }
---
+++ src/mouse-control.h
@@ -0,0 +1,9 @@
+#ifndef MOUSE_CONTROL_H
+#define MOUSE_CONTROL_H
+
+#include <Arduino.h>
+
+void setupMouse(); // Chạy 1 lần ở setup
+void loopMouse();  // Chạy liên tục ở loop
+
+#endif
